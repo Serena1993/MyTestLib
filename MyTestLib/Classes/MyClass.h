af62e61d5991f9efe3ac483d43c8473488937bdb
Serena_R <rui.li07@ele.me>
2017-08-03 17:56:53 +0800

    first commit

---
+++ MyTestLib/Classes/MyClass.h
@@ -0,0 +1,13 @@
+//
+//  MyClass.h
+//  Pods
+//
+//  Created by rui LI on 2017/8/3.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyClass : UIView
+
+@end
---
+++ MyTestLib/Classes/MyClass.m
@@ -0,0 +1,21 @@
+//
+//  MyClass.m
+//  Pods
+//
+//  Created by rui LI on 2017/8/3.
+//
+//
+
+#import "MyClass.h"
+
+@implementation MyClass
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
